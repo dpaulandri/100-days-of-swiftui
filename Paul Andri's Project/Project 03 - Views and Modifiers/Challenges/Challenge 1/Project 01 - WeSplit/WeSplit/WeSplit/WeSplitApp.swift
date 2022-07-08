7f72c0535adc2b091aa14b9fd3c5add548ced796
D. Paul Andri <85876318+dpaulandri@users.noreply.github.com>
2022-07-08 04:44:34 -0400

    Uploaded Day 24 "Challenge 1" Xcode Project file
    Revisited "Project 1 - WeSplit", added Conditional Modifier using Ternary Conditional Operator.

---
+++ Paul Andri's Project/Project 03 - Views and Modifiers/Challenges/Challenge 1/Project 01 - WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Paul Andri on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
