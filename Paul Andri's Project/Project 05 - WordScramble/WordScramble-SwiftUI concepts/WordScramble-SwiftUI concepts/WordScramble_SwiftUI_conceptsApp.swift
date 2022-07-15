5d5d55f0163e189ab1695a58c61352c2ea5413b9
D. Paul Andri <85876318+dpaulandri@users.noreply.github.com>
2022-07-15 07:55:38 -0400

    Uploaded Day 29 SwiftUI concepts Xcode files for Project 5 - WordScramble

---
+++ Paul Andri's Project/Project 05 - WordScramble/WordScramble-SwiftUI concepts/WordScramble-SwiftUI concepts/WordScramble_SwiftUI_conceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_SwiftUI_conceptsApp.swift
+//  WordScramble-SwiftUI concepts
+//
+//  Created by Dennis P. Andri on 07/15/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_SwiftUI_conceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
